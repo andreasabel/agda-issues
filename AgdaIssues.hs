885150c9782441130ad0e19037ccf3aab73ab08a
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-06-06 17:21:47 +0200

    Eliminated hard-coded filenames and dates, refactored

--- AgdaIssues.hs
+++ AgdaIssues.hs
@@ -4,7 +4,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TupleSections #-}
 
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
+-- {-# OPTIONS_GHC -Wno-unused-top-binds #-}
+
+module Main
+  ( main     -- payload
+  , v, test  -- for testing
+  ) where
 
 import GHC.Exts (fromList)
 import GHC.Generics
@@ -17,7 +22,7 @@ import Data.Maybe
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Time
-import Data.Time.Format.ISO8601 (iso8601ParseM)
+import Data.Time.Format.ISO8601 (iso8601ParseM, iso8601Show)
 
 -- For plotting
 import Data.Colour
@@ -25,10 +30,37 @@ import Data.Colour.Names
 import Graphics.Rendering.Chart.Easy
 import Graphics.Rendering.Chart.Backend.Cairo
 
+------------------------------------------------------------------------
+-- * Configuration
+------------------------------------------------------------------------
+
+-- | Date when migration to @github@ was complete.
+--   We only consider issues reported after the migration.
+--   For the others, the @created_at@ date is not correct.
+
+migrationDate :: UTCTime
+migrationDate = timeFromString "2015-08-08T23:59:59+00:00"
+
+migrationDay :: String
+migrationDay = "2015-08-08"
+
+migrationYear :: String
+migrationYear = "2015"
+
+-- | Filename prefix for generated charts.
+
+pngPrefix :: String
+pngPrefix = "agda-issues-"
+
+------------------------------------------------------------------------
+-- * Types
+------------------------------------------------------------------------
+
 -- | Bug database as obtained from github.
 
 data AgdaIssues = AgdaIssues
   { agda :: Map Int Issue  -- ^ Maps issue number to 'Issue'.
+      -- Not IntMap, because this has a different encoding/decoding
   } deriving (Generic, Show)
 
 -- | Issue information as obtained from github.
@@ -47,6 +79,197 @@ instance FromJSON Issue where
 instance ToJSON   AgdaIssues where
 instance FromJSON AgdaIssues where
 
+-- * Main
+------------------------------------------------------------------------
+
+-- | Main program spaghetti code.
+
+main :: IO ()
+main = do
+
+  -- Parse JSON file.
+  ------------------------------------------------------------------------
+
+  issues :: AgdaIssues <- do
+    t <- parseFile "agdaissues.json"
+    case fromJSON t of
+        Error   s -> error s
+        Success x -> return x
+
+  -- List of issues
+  let is :: [(Int,Issue)]
+      is = Map.toList $ agda issues
+
+  -- -- Latest issue
+  -- let latestIssue :: Issue
+  --     latestIssue = maybe undefined snd $ Map.lookupMax issues
+
+  -- Chart for lifetime of open issues.
+  ------------------------------------------------------------------------
+
+  now <- getCurrentTime
+
+  -- Today, e.g. 2022-06-06
+  let nowDay :: String
+      nowDay = iso8601Show $ utctDay now
+      nowYear :: String
+      nowYear = take 4 $ nowDay  -- TODO in year 9999: fixme
+
+  -- UNUSED:
+  -- let openissues = filter ((not . is_pull_request &&? isNothing . closed_at) . snd) is
+
+  let ageInDays = round . (/ nominalDay) . diffUTCTime now
+      maxAge    = ageInDays migrationDate
+
+      -- We consider only issues opened after the migration to github
+      recentissues = filter ((not . is_pull_request &&? (migrationDate <) . created_at) . snd) is
+      recentopenissues = filter (isNothing . closed_at . snd) recentissues
+
+      -- Recent open issues sorted by age in days
+      ageopenissues :: [Int]
+      ageopenissues = sort $ map (ageInDays . created_at . snd) recentopenissues
+
+      -- Number of issues older than x days
+      ageByNo :: [(Int,Int)]
+      ageByNo = zip ageopenissues [1..]
+
+  -- putStrLn $ "Open issues: " ++ show (length openissues)
+  -- forM_ openissues $ \ (no, i) -> print no
+  -- mapM_ print ageopenissues
+  -- mapM_ print ageByNo
+
+  let dateRange = unwords [ migrationDay, "to", nowDay ]
+  let pngSuffix = concat [ "-", migrationYear, "-", nowYear, ".png" ]
+
+  toFile def (concat [pngPrefix, "age-open", pngSuffix]) $ do
+    layout_title .= unwords [ "Open issue age (cumulative),", dateRange ]
+    plotBarsRedGreen $ bars ["days"] $
+      map (\ (age, n) -> (age, [n])) ageByNo
+
+  -- Chart for lifetime of closed issues.
+  ------------------------------------------------------------------------
+
+  -- Issues reported after migration and closed
+  let recentclosedissues =  filter (isJust . closed_at . snd) recentissues
+
+  -- Recent closed issues sorted by age in days
+  let ageclosedissues :: [Int]
+      ageclosedissues = sort $
+        map (round . (/ nominalDay)
+                   . (\ i -> diffUTCTime (fromJust $ closed_at i) (created_at i))
+                   . snd)
+            recentclosedissues
+
+      -- Number of issues that lived longer than x days
+      closedAgeByNo :: [(Int,Int)]
+      closedAgeByNo = zip ageclosedissues [1..]
+
+  toFile def (concat [pngPrefix, "age-closed", pngSuffix]) $ do
+    layout_title .= unwords [ "Closed issue age (cumulative),", dateRange ]
+    -- point_border_width .= 0.0 -- NOT POSSIBLE
+    -- setColors [opaque limegreen]
+    plotBarGreen $ bars ["days"] $
+      map (\ (age, n) -> (age, [n])) closedAgeByNo
+
+  toFile def (concat [pngPrefix, "age-closed-log", pngSuffix]) $ do
+    layout_title .= unwords [ "Closed issue age logarithmically (cumulative),", dateRange ]
+    plotBarGreen $ bars ["days"] $
+      map (\ (age, n) -> (LogValue (fromIntegral age), [n])) closedAgeByNo
+
+  -- Plot open issues vs. total issues over time.
+  ------------------------------------------------------------------------
+
+  -- Sort issues by birth age.
+  let issuesByAge = sortOn created_at $ map snd recentissues
+      initial :: (Int, Int, Int)
+      initial = (0, 0, 0)
+
+      -- UNUSED:
+      -- stepOT (_age, open, total) i =
+      --   ( ageInDays $ created_at i
+      --   , if isNothing (closed_at i) then open + 1 else open
+      --   , total + 1
+      --   )
+      -- openVsTotal = scanl stepOT initial issuesByAge
+
+      stepOC (_age, open, closed) i =
+        ( ageInDays $ created_at i
+        , if isNothing (closed_at i) then open + 1 else open
+        , if isJust (closed_at i) then closed + 1 else closed
+        )
+      openVsClosed = scanl stepOC initial issuesByAge
+
+  toFile def (concat [pngPrefix, "open-vs-closed", pngSuffix]) $ do
+    layout_title .= unwords [ "Open vs closed issues,", dateRange ]
+    -- point_border_width .= 0.0 -- NOT POSSIBLE
+    -- setColors [opaque limegreen]
+    plotBarsRedGreen $ bars ["open issues", "closed issues"] $
+      map (\ (t, o, c) -> (maxAge - t, [o,c])) openVsClosed
+
+
+  -- Print some basic statistics.
+  ------------------------------------------------------------------------
+
+  let ni = length recentissues
+      ci = length recentclosedissues
+      oi = length recentopenissues
+  putStrLn $ concat [ "Only counting issues after migration to github on ", migrationDay, "." ]
+  putStrLn $ "Total  issues: " ++ show ni
+  putStrLn $ "Closed issues: " ++ show ci
+                ++ " (" ++ show (100 * ci `div` ni) ++ "%)"
+  putStrLn $ "Open   issues: " ++ show oi
+                ++ " (" ++ show (100 * oi `div` ni) ++ "%)"
+
+  where
+    plotBarsRedGreen b = plot $ barsRedGreen <$> b
+    plotBarGreen     b = plot $ barGreen     <$> b
+
+
+solidGreen, solidRed :: FillStyle
+solidGreen = FillStyleSolid $ opaque limegreen
+solidRed   = FillStyleSolid $ opaque crimson
+
+barGreen :: PlotBars a Int -> Plot a Int
+barGreen =
+  plotBars
+  . set plot_bars_style BarsStacked
+  . set plot_bars_item_styles (map (,Nothing) [ solidGreen ])
+
+barsRedGreen :: PlotBars a Int -> Plot a Int
+barsRedGreen =
+  plotBars
+  . set plot_bars_style BarsStacked
+  . set plot_bars_item_styles (map (,Nothing) [ solidRed, solidGreen ])
+
+
+------------------------------------------------------------------------
+-- * Auxiliary functions
+------------------------------------------------------------------------
+
+-- | Pointwise conjunction.
+
+(&&?) :: (Issue -> Bool) -> (Issue -> Bool) -> Issue -> Bool
+p &&? q = \ x -> p x && q x
+
+infixl 2 &&?
+
+-- | Time parsing from github/python formatted zoned-time values.
+
+timeFromString :: String -> UTCTime
+timeFromString =
+  maybe (error "not a valid time value") zonedTimeToUTC . iso8601ParseM
+
+-- | Read a JSON 'Value' from a file.
+
+parseFile :: FilePath -> IO Value
+parseFile f = do
+  s <- BS.readFile f
+  return $ fromMaybe (error "json parse failed") $ decode s
+
+------------------------------------------------------------------------
+-- * Testing
+------------------------------------------------------------------------
+
 -- | A sample issue listing.
 
 v :: AgdaIssues
@@ -59,19 +282,6 @@ v = AgdaIssues $ Map.fromList
        -- , labels          = ["auto-migrated", "bug", "priority-low"]
        })
   ]
-
--- | Time parsing from github/python formatted zoned-time values.
-
-timeFromString :: String -> UTCTime
-timeFromString =
-  maybe (error "not a valid time value") zonedTimeToUTC . iso8601ParseM
-
--- | Pointwise conjunction.
-
-(&&?) :: (Issue -> Bool) -> (Issue -> Bool) -> Issue -> Bool
-p &&? q = \ x -> p x && q x
-
-infixl 2 &&?
 
 -- | An experiment to produce JSON from 'Value' representation.
 
@@ -94,152 +304,3 @@ test = encode
     -- , ("labels",) . Array . fromList $ [ "auto-migrated" , "bug", "priority-low" ]
     ]
   ]
-
--- | Date when migration to @github@ was complete.
---   We only consider issues reported after the migration.
---   For the others, the @created_at@ date is not correct.
-
-migrationDate :: UTCTime
-migrationDate = timeFromString "2015-08-08T23:59:59+00:00"
-
--- | Read a JSON 'Value' from a file.
-
-parseFile :: FilePath -> IO Value
-parseFile f = do
-  s <- BS.readFile f
-  return $ fromMaybe (error "json parse failed") $ decode s
-
--- | Main program spaghetti code.
-
-main :: IO ()
-main = do
-
-  -- Parse JSON file.
-  ------------------------------------------------------------------------
-
-  issues :: AgdaIssues <- do
-    t <- parseFile "agdaissues.json"
-    case fromJSON t of
-        Error   s -> error s
-        Success x -> return x
-
-  -- List of issues
-  let is :: [(Int,Issue)]
-      is = Map.toList $ agda issues
-
-  -- Chart for livetime of open issues.
-  ------------------------------------------------------------------------
-
-  now <- getCurrentTime
-
-  let openissues = filter ((not . is_pull_request &&? isNothing . closed_at) . snd) is
-
-      ageInDays = round . (/ nominalDay) . diffUTCTime now
-      maxAge    = ageInDays migrationDate
-
-      -- We consider only issues opened after the migration to github
-      recentissues = filter ((not . is_pull_request &&? (migrationDate <) . created_at) . snd) is
-      recentopenissues = filter (isNothing . closed_at . snd) recentissues
-
-      -- Recent open issues sorted by age in days
-      ageopenissues :: [Int]
-      ageopenissues = sort $ map (ageInDays . created_at . snd) recentopenissues
-
-      -- Number of issues older than x days
-      ageByNo :: [(Int,Int)]
-      ageByNo = zip ageopenissues [1..]
-
-  -- putStrLn $ "Open issues: " ++ show (length openissues)
-  -- forM_ openissues $ \ (no, i) -> print no
-  -- mapM_ print ageopenissues
-  -- mapM_ print ageByNo
-
-  toFile def "agda-issues-age-open-2015-2022.png" $ do
-    layout_title .= "Open issue age (cumulative), 2015-08-08 to 2022-06-06"
-    plot $ plotBars
-         . set plot_bars_style BarsStacked
-         . set plot_bars_item_styles
-             [ (FillStyleSolid $ opaque crimson  , Nothing)
-             , (FillStyleSolid $ opaque limegreen, Nothing)
-             ]
-           <$> bars ["days"] (map (\ (age, n) -> (age, [n])) ageByNo)
-
-  -- Chart for lifetime of closed issues.
-  ------------------------------------------------------------------------
-
-  -- Issues reported after migration and closed
-  let recentclosedissues =  filter (isJust . closed_at . snd) recentissues
-
-      -- Recent closed issues sorted by age in days
-      ageclosedissues :: [Int]
-      ageclosedissues = sort $ map (round . (/ nominalDay) . (\ i -> diffUTCTime (fromJust . closed_at $ i) (created_at i)) . snd) recentclosedissues
-
-      -- Number of issues that lived longer than x days
-      closedAgeByNo :: [(Int,Int)]
-      closedAgeByNo = zip ageclosedissues [1..]
-
-  toFile def "agda-issues-age-closed-2015-2022.png" $ do
-    layout_title .= "Closed issue age (cumulative), 2015-08-08 to 2022-06-06"
-    -- point_border_width .= 0.0 -- NOT POSSIBLE
-    -- setColors [opaque limegreen]
-    plot $ plotBars
-         . set plot_bars_style BarsStacked
-         . set plot_bars_item_styles
-             [ (FillStyleSolid $ opaque limegreen, Nothing)
-             ]
-           <$> bars ["days"] (map (\ (age, n) -> (age, [n])) closedAgeByNo)
-
-  toFile def "agda-issues-age-closed-log-2015-2022.png" $ do
-    layout_title .= "Closed issue age logarithmically (cumulative), 2015-08-08 to 2022-06-06"
-    plot $ plotBars
-         . set plot_bars_style BarsStacked
-         . set plot_bars_item_styles
-             [ (FillStyleSolid $ opaque limegreen, Nothing)
-             ]
-           <$> bars ["days"] (map (\ (age, n) -> (LogValue (fromIntegral age), [n])) closedAgeByNo)
-
-  -- Plot open issues vs. total issues over time.
-  ------------------------------------------------------------------------
-
-  -- Sort issues by birth age.
-  let issuesByAge = sortOn created_at $ map snd recentissues
-      initial :: (Int, Int, Int)
-      initial = (0, 0, 0)
-      stepOT (_age, open, total) i =
-        ( ageInDays $ created_at i
-        , if isNothing (closed_at i) then open + 1 else open
-        , total + 1
-        )
-      openVsTotal = scanl stepOT initial issuesByAge
-      stepOC (_age, open, closed) i =
-        ( ageInDays $ created_at i
-        , if isNothing (closed_at i) then open + 1 else open
-        , if isJust (closed_at i) then closed + 1 else closed
-        )
-      openVsClosed = scanl stepOC initial issuesByAge
-
-  toFile def "agda-issues-open-vs-closed-2015-2022.png" $ do
-    layout_title .= "Open vs closed issues 2015-08-08 to 2022-06-06"
-    -- point_border_width .= 0.0 -- NOT POSSIBLE
-    -- setColors [opaque limegreen]
-    plot $ plotBars
-         . set plot_bars_style BarsStacked
-         . set plot_bars_item_styles
-             [ (FillStyleSolid $ opaque crimson  , Nothing)
-             , (FillStyleSolid $ opaque limegreen, Nothing)
-             ]
-         <$> bars ["open issues", "closed issues"] (map (\ (t, o, c) -> (maxAge - t, [o,c])) openVsClosed)
-
-
-  -- Print some basic statistics.
-  ------------------------------------------------------------------------
-
-  let ni = (length recentissues)
-      ci = (length recentclosedissues)
-      oi = (length recentopenissues)
-  putStrLn $ "Only counting issues after migration to github on 2015-08-08."
-  putStrLn $ "Total  issues: " ++ show ni
-  putStrLn $ "Closed issues: " ++ show ci
-                ++ " (" ++ show (100 * ci `div` ni) ++ "%)"
-  putStrLn $ "Open   issues: " ++ show oi
-                ++ " (" ++ show (100 * oi `div` ni) ++ "%)"
